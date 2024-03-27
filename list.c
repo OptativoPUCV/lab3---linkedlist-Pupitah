397a844fafbdd469e9fe5a21210b787808d6935a
Your Name <you@example.com>
2024-03-27 19:04:51 +0000

    new try

--- list.c
+++ list.c
@@ -52,7 +52,6 @@ void * nextList(List * list) {
 void * lastList(List * list) {
   if (list==NULL || list->tail ==NULL) return NULL;    //Condición lista vacía.
   list->current = list->tail;
-  list->current->next->next;
   return (void *) list->current->data;
 }
 
